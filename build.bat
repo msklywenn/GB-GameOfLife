d2afcd9225c781374982f712ef18b3899232681f
Daniel Borges <msk@lywenn.eu.org>
2019-01-06 22:55:46 +0100

    added hardware.inc to project

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\main.o         Code\main.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\data.o         Code\data.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\automata.o     Code\automata.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\render.o       Code\render.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\utils.o        Code\utils.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\nintendo-out.o Code\nintendo-out.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\edit.o         Code\edit.asm
-..\..\Assembler\rgbasm %* -i ..\..\Include\ -i Graphics\ -i Code\ -o Build\joypad.o       Code\joypad.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\main.o         Code\main.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\data.o         Code\data.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\automata.o     Code\automata.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\render.o       Code\render.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\utils.o        Code\utils.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\nintendo-out.o Code\nintendo-out.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\edit.o         Code\edit.asm
+..\..\Assembler\rgbasm %* -i Graphics\ -i Code\ -o Build\joypad.o       Code\joypad.asm
 ..\..\Assembler\rgblink -n rom.sym -w -t -o rom.gb -d Build/main.o Build/data.o Build/automata.o Build/render.o Build/utils.o Build/nintendo-out.o Build/edit.o Build/joypad.o
 ..\..\Assembler\rgbfix -t "Game of Life" -v -p 0 rom.gb
